0aca26316da795fbc903ff625e16a42a09907e7e
ud123df <147704288+ud123df@users.noreply.github.com>
2026-06-20 13:17:48 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,8 +15,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://aidevops.netlify.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
